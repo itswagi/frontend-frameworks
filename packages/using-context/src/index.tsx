af2c408dbd19c8ce67d7c069d56ba9f68fd992f2
Wajahat <wzia.bscs16seecs@seecs.edu.pk>
2022-04-25 02:51:24 +0500

    Added styled components and cleaned up app.tsx

--- packages/using-context/src/index.tsx
+++ packages/using-context/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppProvider } from './context/AppContext';
+import { ThemeProvider } from "styled-components";
+import { theme } from './styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </AppProvider>
   </React.StrictMode>
 );
